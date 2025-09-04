7cb0790fb9cff94ec866fadf0c013bb573cd2936
BolarinwaSheu30 <bolarinwasheu42@gmail.com>
2025-09-04 15:51:54 +0100

    web: gunicorn edutrack.wsgi

--- edutrack/settings.py
+++ edutrack/settings.py
@@ -53,6 +53,7 @@ REST_FRAMEWORK = {
 }
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middlewear.common.commonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
