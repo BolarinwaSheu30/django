e97eacf684072806470464d9cded8b73a4f9a325
BolarinwaSheu30 <bolarinwasheu42@gmail.com>
2026-02-21 13:30:14 +0100

    Update build.sh for Render

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 set -o errexit
 
+set -o xtrace
+
+echo "Upgrading pip.."
 pip install --upgrade pip
+
+
+echo "Installing dependencies..." 
 pip install -r requirements.txt
 
+echo "Collecting static files..."
 python manage.py collectstatic --no-input --clear
+
+echo "Running migrations..."
 python manage.py migrate
+
+echo "Build completed successfully!"
